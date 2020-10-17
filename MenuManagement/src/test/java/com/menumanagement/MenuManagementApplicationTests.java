d01b30194bab19958ee3543ea4ca34c44b8cdbb2
vesel <vesel@LAPTOP-DSSGLKK0.mshome.net>
2020-10-18 00:39:03 +0300

    Initial Project Generated With Spring Initializr

---
+++ MenuManagement/src/test/java/com/menumanagement/MenuManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.menumanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
